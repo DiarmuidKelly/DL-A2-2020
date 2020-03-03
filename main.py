643d0a6964e4c4a72454e78ef0f388211b9806c6
diarmuidk <diarmuid100@gmail.com>
2020-03-03 14:52:10 +0100

    Print running duration

--- main.py
+++ main.py
@@ -163,11 +163,12 @@ def plot_durations():
     plt.xlabel('Episode')
     plt.ylabel('Duration')
     plt.plot(durations_t.numpy())
+    print(durations_t)
+
     # Take 100 episode averages and plot them too
     if len(durations_t) >= 100:
         means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
         means = torch.cat((torch.zeros(99), means))
-        print(means)
         plt.plot(means.numpy())
 
     plt.pause(0.001)  # pause a bit so that plots are updated
